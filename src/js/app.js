3858b2b1e24b3bf149025cb9bd36da241f80b33b
MonicaBarreraC <u653058995@byui.edu>
2024-02-15 22:24:32 -0600

    Getting Random Recipe information Home Page (still need to separate things in modules js)

--- src/js/app.js
+++ src/js/app.js
@@ -11,20 +11,42 @@ async function convertToJson(res){
 async function getData() {
     const response = await fetch(url);
     const data = await convertToJson(response);
+
+    // Get Random Recipe
+    const random = getRandomRecipe(data.Result.length);
+    console.log(random);
+    console.log(data.Result[random]);
+    const recipe = data.Result[random];
+    console.log(`name: ${recipe.name}`);
+    //recipeTemplate(data.Result[])
+
+    const selector = "main";
+    const element = document.querySelector(selector);
+    element.insertAdjacentHTML(
+        "beforeend",
+        recipeTemplate(recipe)
+      );
+
     console.log(data.Result);
     //return data.Result;
+}
+
+function getRandomRecipe(max) {
+    const random =  Math.floor(Math.random() * max);
+    //console.log(random);
+    return random;
 }
 
 function recipeTemplate(recipe){
     
     return `<section class="recipe-details">
-        <h3>${recipe.name}</h3>
-        <h2>${recipe.source}</h2>
         <img
-        src=""
-        alt="${recipe.name}"
+            src=""
+            alt="${recipe.name}"
         />
-        <p class="recipe__tags">${recipe.tags[0]}</p>
+        <h1>${recipe.name}</h1>
+        <p>${recipe.source}</p>
+        <p class="recipe__tags">${recipe.tags}</p>
         <p class="recipe__ingredients">
             ${recipe.ingredients}
         </p>
