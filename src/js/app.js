4dc67ee6f7e4b78648522d5eb806e2c6d6639089
MonicaBarreraC <u653058995@byui.edu>
2024-02-17 15:13:55 -0600

    Full recipe function calling JSON is working

--- src/js/app.js
+++ src/js/app.js
@@ -24,7 +24,7 @@ async function getData() {
     const element = document.querySelector(selector);
     element.insertAdjacentHTML(
         "beforeend",
-        recipeTemplate(recipe)
+        fullRecipeTemplate(recipe)
       );
 
     console.log(data.Result);
@@ -37,27 +37,46 @@ function getRandomRecipe(max) {
     return random;
 }
 
-function recipeTemplate(recipe){
+function displayList(list){
+    let html = "";
+    list.forEach(element => {
+        if(element == "<hr>\r"){
+            html += element;
+        } else {
+            html += `<li>${element}</li>`;
+        }
+    });
+    return html;
+}
+
+function fullRecipeTemplate(recipe){
     
-    return `<section class="recipe-details">
-        <img
-            src=""
+    return `<section class="full-recipe">
+        <div class="img-title">
+            <img
+            src="/images/placeholder.jpg"
             alt="${recipe.name}"
-        />
-        <h1>${recipe.name}</h1>
-        <p>${recipe.source}</p>
-        <p class="recipe__tags">${recipe.tags}</p>
-        <p class="recipe__ingredients">
-            ${recipe.ingredients}
-        </p>
+            />
+            <div>
+                <h1>${recipe.name}</h1>
+                <p>${recipe.source}</p>
+                <p><span class="stars">&#9733&#9733&#9733&#9733&#9734</span> (4.5)</p>
+                <button id="addToFav" data-id="${recipe.id}">❤️</button>
+            </div>
+        </div>
+        <h3>Ingredients</h3>
+            <ul class="recipe__ingredients">
+                ${displayList(recipe.ingredients)}
+            </ul>
+        <h3>Instructions</h3>
         <p class="recipe__instructions">
             ${recipe.instructions}
         </p>
-        <div class="recipe-fav__add">
-        <button id="addToFav" data-id="${recipe.id}">❤️</button>
-        </div>
-    </section>`;
+        </section>`;
 }
 
 getData();
 
+/*⭐⭐⭐⭐⭐
+<p class="recipe__tags">${recipe.tags}</p>
+<section class="recipe-details">*/
